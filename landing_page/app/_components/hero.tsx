29a1add5cc7ea3cc2091a52ac5401193e92c2c2a
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-04-28 10:56:04 -0300

    v1 do hero

--- landing_page/app/_components/hero.tsx
+++ landing_page/app/_components/hero.tsx
@@ -1,4 +1,5 @@
-
+import Image from "next/image"
+import HeroImage from "../../public/eu.png"
 
 export function Hero () {
     return (
@@ -6,28 +7,37 @@ export function Hero () {
             
             <div className="absolute inset-0 bg-black/50"></div>
 
-            <div className="relative z-10 mt-30">
+            <div className="container mx-auto mt-30 px-4 py-16 relative">
 
-                <article className="flex flex-col gap-2 p-2">
+                <div className="relative z-10">
 
-                    <h1 className="text-4xl md:text-6xl font-bold">Olá, eu sou o Matias.</h1>
+                    <article className="grid grid-cols-1 lg:grid-cols-2 gap-8">
 
-                    <p className="text-2xl md:text-4xl">Meu lema é: "Tentando ser melhor".</p>
+                        <div className="flex flex-col gap-3">
 
-                    <div className="mt-3">
-                        <a href="#sobre_mim"
-                        className="bg-white px-5 py-2 text-black rounded-md font-semibold">
-                        Mais sobre mim.
-                        </a>
-                    </div>
-                    <div className="mt-3">
-                        <a href="#projetos" 
-                        className="bg-white px-5 py-2 text-black rounded-md font-semibold">
-                            Meus projetos.
-                        </a>
-                    </div>
+                            <h1 className="text-4xl md:text-6xl font-bold">Olá, eu sou o Matias.</h1>
 
-                </article>
+                            <p className="text-2xl md:text-3xl">Meu lema é: "Tentando ser melhor".</p>
+
+                            <div className="mt-3">
+                                <a href="#sobre_mim"
+                                className="bg-white px-5 py-2 text-black rounded-md font-semibold">
+                                Mais sobre mim.
+                                </a>
+                            </div>
+                            <div className="mt-3">
+                                <a href="#projetos" 
+                                className="bg-white px-5 py-2 text-black rounded-md font-semibold">
+                                    Meus projetos.
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="hidden md:block h-full">
+                            <Image src={HeroImage} alt="Foto do Matias" />
+                        </div>
+                    </article>
+                </div>
             </div>
         </section>
     )
