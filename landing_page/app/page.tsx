db0c4f82c17223bd4efc7898461925e0818c385c
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-04-28 15:52:42 -0300

    Importando o componente about

--- landing_page/app/page.tsx
+++ landing_page/app/page.tsx
@@ -1,3 +1,4 @@
+import { About } from "./_components/about";
 import {Hero} from "./_components/hero"
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     
     <main>
       <Hero/>
+      <About/>
     </main>
    
   );
