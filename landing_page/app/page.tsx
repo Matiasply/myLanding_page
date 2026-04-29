74933ce35f0467189c6b55af8686775feb701987
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-04-29 15:23:39 -0300

    Adicionando component de projects

--- landing_page/app/page.tsx
+++ landing_page/app/page.tsx
@@ -1,5 +1,6 @@
+import { Hero } from "./_components/hero";
 import { About } from "./_components/about";
-import {Hero} from "./_components/hero"
+import { Projects } from "./_components/projects";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
       <Hero/>
       <About/>
+      <Projects/>
     </main>
    
   );
