23ba85dfd2a3c94318ec9ba2c09721d3fd3d452b
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-04-29 14:22:50 -0300

    Adicionando fonte Open Sans

--- landing_page/app/layout.tsx
+++ landing_page/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Open_Sans } from "next/font/google"
+
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'],
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +30,10 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="pt-br"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`min-h-full flex flex-col ${openSans.className}`}>{children}</body>
     </html>
   );
 }
