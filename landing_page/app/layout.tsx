de5f584d2eac15c131aa570ffec3d7d3060fb5c2
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-05-01 11:03:33 -0300

    Adicionando scroll-smooth para navegação suave

--- landing_page/app/layout.tsx
+++ landing_page/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html
       lang="pt-br"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
     >
       <body className={`min-h-full flex flex-col ${openSans.className}`}>
         {children}
