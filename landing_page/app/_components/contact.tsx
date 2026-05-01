e1ef8ce963f31737aa9374162357196cbdcbffaa
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-05-01 08:46:59 -0300

    Finalizando o rodapé da página

--- landing_page/app/_components/contact.tsx
+++ landing_page/app/_components/contact.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image"
-import GitIcon from "../../public/githubIIIpng.png"
+import GitIcon from "../../public/githubIII.png"
 import InstaIcon from "../../public/instagram.png"
 import LinkeIcon from "../../public/linkedin.png"
+import EmailIcon from "../../public/email.png"
 
 export function Contacts () {
 
@@ -12,26 +13,42 @@ export function Contacts () {
 
             <div className="grid grid-cols-3 gap-5">
 
-                <a href="https://github.com/Matiasply" className="mx-auto hover:scale-110 duration-300">
+                <a href="https://github.com/Matiasply" className="mx-auto hover:scale-105 duration-300">
 
                     <Image src={GitIcon} alt="Logo do github" className="w-40 h-40"/>
 
                 </a>
 
-                <a href="https://www.instagram.com/matiasmonteirodale/" className="mx-auto hover:scale-110 duration-300">
+                <a href="https://www.instagram.com/matiasmonteirodale/" className="mx-auto hover:scale-105 duration-300">
 
                     <Image src={InstaIcon} alt="Logo do instagram" className="w-70 h-50"/>
 
                 </a>
 
                 <a href="https://www.linkedin.com/in/matias-monteiro-de-ara%C3%BAjo-bbb4271bb/" 
-                className="mx-auto hover:scale-110 duration-300">
+                className="mx-auto hover:scale-105 duration-300">
 
                     <Image src={LinkeIcon} alt="Logo do linkedin" className="w-70 h-50"/>
 
                 </a>
 
             </div>
+
+            <h4 className="text-2xl text-center text-white">Me mande um email.</h4>
+
+            <a href="mailto:matiasmonteiroaraujo@gmail.com">
+
+                <Image src={EmailIcon} alt="Símbolo de email" className="mx-auto h-50 w-70 hover:scale-105 duration-300"/>
+
+            </a>
+
+            <p className="text-white text-center"> 
+                Projeto desenvolvido por mim, Matias, com o intuito
+                de desenvolver minhas habilidades em Nextjs.
+                <br />
+                &copy; Direitos Reservados.
+                
+            </p>
 
 
         </section>
