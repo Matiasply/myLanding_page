31b27acc6b86543609fc38971875d60bab40023e
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-05-02 12:00:54 -0300

    Props de sizes na imagem

--- landing_page/app/_components/about.tsx
+++ landing_page/app/_components/about.tsx
@@ -21,7 +21,7 @@ export function About() {
             <div data-aos="flip-left">
 
             <div className="relative w-full h-100 rounded-3xl overflow-hidden">
-                <Image src={provImage} alt="Imagem provisória" fill quality={100} priority className="object-cover hover:scale-110 duration-300"/>
+                <Image src={provImage} alt="Imagem provisória" fill sizes="(max-width: 1024px) 100vw, 33vw" quality={100} priority className="object-cover hover:scale-110 duration-300"/>
             </div>
 
             </div>
